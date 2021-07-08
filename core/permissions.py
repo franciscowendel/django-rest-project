04740c12b8424ab499667ae85e088e203849f68d
franciscowendel <iamfranciscowendel@gmail.com>
2021-07-07 22:14:34 -0300

    Arquivo 'permissions.py' editado

--- core/permissions.py
+++ core/permissions.py
@@ -1,9 +1,27 @@
 from rest_framework import permissions
 
 
-class EhSuperUser(permissions.BasePermission):
+class EhSuperUserDelete(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'DELETE':
+            if request.user.is_super_user:
+                return True
+            return False
+        return True
+
+
+class EhSuperUserPut(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'PUT':
+            if request.user.is_super_user:
+                return True
+            return False
+        return True
+
+
+class EhSuperUserPost(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
             if request.user.is_super_user:
                 return True
             return False
